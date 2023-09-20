f5fd0fc5aabff75a792bdca3f54a1984291f1e98
satriayudhia <satria.yudhiap@gmail.com>
2023-09-20 13:42:30 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,163 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1a-utama",
+      "name": "1A. UTAMA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.4905958861095385,
+        "pitch": 0.20589699834116892,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7576260656859226,
+          "pitch": 0.35317720842893685,
+          "rotation": 1.5707963267948966,
+          "target": "1-1b-kamar-mandi"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1b-kamar-mandi",
+      "name": "1B. KAMAR MANDI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.2307713122081374,
+        "pitch": 0.07770502401130841,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7690081619372116,
+          "pitch": 0.24575993302179278,
+          "rotation": 0,
+          "target": "0-1a-utama"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2a-utama",
+      "name": "2A. UTAMA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.4886921905584103,
+        "pitch": 0.2465124899669071,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6559267184280397,
+          "pitch": 0.25851491178075037,
+          "rotation": 4.71238898038469,
+          "target": "3-2b-kamar-mandi"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2b-kamar-mandi",
+      "name": "2B. KAMAR MANDI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.08920571732415539,
+        "pitch": 0.08038450759789839,
+        "fov": 1.3826135306839293
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.803999294676739,
+          "pitch": 0.3276611969623051,
+          "rotation": 0,
+          "target": "2-2a-utama"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
